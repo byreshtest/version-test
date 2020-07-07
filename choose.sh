599f939826fcb186835f4b0a6303f63ab72e4634
byreswara <byreswarareddy.k@gmail.com>
2020-07-07 16:01:36 +0000

    modified

--- choose.sh
+++ choose.sh
@@ -1,31 +1,30 @@
-#!/bin/bash
+#!/bin/bash +X
 
-major=`awk '{print $1 }' $component.txt` 
-minor=`awk '{print $2 }' $component.txt`
+export major=`awk '{print $1 }' $component.txt` 
+export minor=`awk '{print $2 }' $component.txt`
 patch=`awk '{print $3 }' $component.txt`
 
 if [ 'patch' = $version ] 
  then
-   patch=$((patch + 1))
+   export patch=$((patch + 1))
    echo VERSION:$major.$minor.$patch
    echo $major $minor $patch > $component.txt
    echo version file updated
  elif [ 'minor' = $version ]
    then
-     minor=$((minor + 1))
-     patch=0
+     export minor=$((minor + 1))
+     export patch=0
      echo VERSION:$major.$minor.$patch
      echo $major $minor $patch > $component.txt
      echo version file updated
  elif [ 'major' = $version ]
    then
-     major=$((major + 1))
-     minor=0
-     patch=0
+     export major=$((major + 1))
+     export minor=0
+     export patch=0
      echo VERSION:$major.$minor.$patch
      echo $major $minor $patch > $component.txt
      echo version file updated
  else
      echo version not selected
 fi
-
